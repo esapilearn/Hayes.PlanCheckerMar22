47c17e474b81b65097bebb4d73b89a5bbe46c46b
Justin Hayes <justinjhayes@gmail.com>
2022-03-16 10:22:07 -0700

    basic view model created

---
+++ ESAPIX_StarterUI1/ViewModels/PlanConstraint.cs
@@ -0,0 +1,28 @@
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class PlanConstraint : BindableBase
+        // Planconstraint is inherited fromthe BindableBase (this is what the : is doing/saying)
+    {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+        private IConstraint _constraint;
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
